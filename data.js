5732eba521e47665c8bb362685a0dd205911faa4
Kathy <kath.scholle@gmail.com>
2023-01-28 10:04:44 -0500

    adds seed data

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [{
+    _id: 'dflk4561',
+    title: 'Shopping List',
+    author: 'Monica',
+    date: '01/27/2023',
+    content: 'pants',
+    userId: '123123'
+},
+{
+    _id: 'rygrhg5465',
+    title: 'Grocery List',
+    author: 'Monica',
+    date: '01/28/2023',
+    content: 'carrots',
+    userId: '123123'
+},
+{
+    _id: 'dfght543',
+    title: 'New Book Ideas',
+    author: 'Joey',
+    date: '01/01/2023',
+    content: {
+        book1: 'Living With My Friends in NYC',
+        book2: 'My Dream As An Actor'
+    },
+    userId: '1234512345',
+},
+{
+    _id: 'dfgrdg1235',
+    title: 'Quotes',
+    author: 'Joey',
+    date: '01/01/2022',
+    content: {
+        Quote1: '1. When give the choice between right and being kind, choose kind.',
+        Quote2: '2. When Life Gives you Lemons, Make Lemonade!'
+    },
+    userId: '1234512345',
+},
+]
